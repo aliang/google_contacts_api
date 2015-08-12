8283d5d665686ec86aea81e2b4bf1db0c718f2ab
Никита Соколов <faucct@gmail.com>
2015-08-12 15:55:05 +0300

    xml builder fixed

--- lib/google_contacts_api/builder.rb
+++ lib/google_contacts_api/builder.rb
@@ -1,15 +1,12 @@
+require 'nokogiri'
+
 module GoogleContactsApi
   class Builder
     def initialize(data)
-      @builder = Nokogiri::XML::Builder.new do |xml|
+      data = data.with_indifferent_access
+      @builder = Nokogiri::XML::Builder.new data.slice('encoding') do |xml|
         @xml = xml
-        data.each_pair do |key, value|
-          if value.kind_of? Hash
-            build_node key, value
-          else
-            xml.method_missing key, value
-          end
-        end
+        build_node 'root', data.except('version', 'encoding')
       end
     end
 
@@ -19,11 +16,17 @@ module GoogleContactsApi
 
     private
     def build_node(name, data)
-      attributes, children = data.partition { |key, value| value.kind_of? Hash }
+      children, attributes = data.partition { |key, value| value.kind_of?(Hash) || value.kind_of?(Array) }
       attributes = Hash[attributes].with_indifferent_access
       @xml.method_missing name, attributes.delete('$t'), attributes do
         children.each do |key, value|
-          build_node key, value
+          if value.kind_of? Array
+            value.each do |v|
+              build_node key, v
+            end
+          else
+            build_node key, value
+          end
         end
       end
     end
